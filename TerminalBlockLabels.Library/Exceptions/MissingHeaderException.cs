c2600c5f8429cdf691bdd1a2789feb8f77871290
Ishmael Moreno <ishmael.a.moreno@gmail.com>
2025-01-21 13:33:43 -0500

    Added MissingHeaderException class.

---
+++ TerminalBlockLabels.Library/Exceptions/MissingHeaderException.cs
@@ -0,0 +1,27 @@
+﻿namespace CleaverBrooks.TerminalStripUtility.Library.Exceptions
+{
+    /// <summary>
+    /// Exception that gets thrown if a CSV file is missing an expecting header.
+    /// </summary>
+    public class MissingHeaderException : Exception
+    {
+        private readonly string missingHeaderName;
+
+        /// <summary>
+        /// Constructor for the <see cref="MissingHeaderException"/> class.
+        /// </summary>
+        /// <param name="missingHeaderName">The name of the header that is missing.</param>
+        public MissingHeaderException(string missingHeaderName)
+        {
+            this.missingHeaderName = missingHeaderName;
+        }
+
+        /// <summary>
+        /// The name of the header that is missing.
+        /// </summary>
+        public string MissingHeaderName
+        {
+            get { return missingHeaderName; }
+        }
+    }
+}
