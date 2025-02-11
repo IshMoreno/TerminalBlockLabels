d13433e3fa1f20f104a6c94f2e58dcb9442c369f
Ishmael Moreno <77425666+IshmaelMorenoH@users.noreply.github.com>
2025-02-10 21:57:51 -0500

    Added FileReaders test folder.

---
+++ TerminalBlockLabels.Tests/FileReaders/AutoCadFileReaderWhenCreateAutoCadFileReader.cs
@@ -0,0 +1,42 @@
+﻿using CleaverBrooks.Orders;
+using CleaverBrooks.TerminalStripUtility.Library.FileReaders;
+
+namespace CleaverBrooks.TerminalStripUtility.Tests.FileReaders
+{
+    /// <summary>
+    /// Class for testing the constructor for the <see cref="AutoCadFileReader"/> class.
+    /// </summary>
+    [TestClass]
+    public class AutoCadFileReaderWhenCreateAutoCadFileReader
+    {
+        [TestMethod]
+        public void ShouldCreateFromFilepath()
+        {
+            string filepath = @"C:\T1234-1-1.csv";
+            AutoCadFileReader autoCadFileReader = new(filepath);
+            Assert.AreEqual(filepath, autoCadFileReader.Filepath);
+        }
+
+        [TestMethod]
+        public void ShouldCreateFromSerialNumberWhenHostPanel()
+        {
+            SerialNumber serialNumber = new("T1234-1-1");
+            bool isHostPanel = true;
+
+            AutoCadFileReader autoCadFileReader = new(serialNumber, isHostPanel);
+
+            Assert.AreEqual(@"\\aqcorp.com\thv\CAD\Submit\Wire Marker Files\T1234-1-1HP.csv", autoCadFileReader.Filepath);
+        }
+
+        [TestMethod]
+        public void ShouldCreateFromSerialNumberWhenNotHostPanel()
+        {
+            SerialNumber serialNumber = new("T1234-1-1");
+            bool isHostPanel = false;
+
+            AutoCadFileReader autoCadFileReader = new(serialNumber, isHostPanel);
+
+            Assert.AreEqual(@"\\aqcorp.com\thv\CAD\Submit\Wire Marker Files\T1234-1-1.csv", autoCadFileReader.Filepath);
+        }
+    }
+}
---
+++ TerminalBlockLabels.Tests/FileReaders/AutoCadFileReaderWhenReadFileAsync.cs
@@ -0,0 +1,74 @@
+﻿using CleaverBrooks.TerminalStripUtility.Library.Exceptions;
+using CleaverBrooks.TerminalStripUtility.Library.FileReaders;
+using CleaverBrooks.TerminalStripUtility.Library.Models;
+
+namespace CleaverBrooks.TerminalStripUtility.Tests.FileReaders
+{
+    /// <summary>
+    /// Class for testing the <see cref="AutoCadFileReader.ReadFileAsync"/> method.
+    /// </summary>
+    [TestClass]
+    public class AutoCadFileReaderWhenReadFileAsync
+    {
+        [TestMethod]
+        public void ShouldThrowInvalidFileException()
+        {
+            string directoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resources\";
+            string filepath = directoryPath + "InvalidAutoCADFile.csv";
+            AutoCadFileReader reader = new(filepath);
+            Assert.ThrowsExceptionAsync<FileNotFoundException>(() => reader.ReadFileAsync());
+        }
+
+        [TestMethod]
+        public async Task ShouldReadValidFile()
+        {
+            string directoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resources\";
+            string filepath = directoryPath + "ValidAutoCADFile.csv";
+            AutoCadFileReader reader = new(filepath);
+            IEnumerable<Wire> wires = await reader.ReadFileAsync();
+            Assert.AreEqual(1, wires.Count());
+            Assert.AreEqual("CP POWER", wires.ElementAt(0).FirstLocation);
+            Assert.AreEqual("CONTROL PANEL", wires.ElementAt(0).SecondLocation);
+            Assert.AreEqual("3111", wires.ElementAt(0).Number);
+            Assert.AreEqual("BLU_16AWG", wires.ElementAt(0).LayerName);
+        }
+
+        [TestMethod]
+        public async Task ShouldReadFileIfTooManyHeaders()
+        {
+            string directoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resources\";
+            string filepath = directoryPath + "AutoCADFileWithTooManyHeaders.csv";
+            AutoCadFileReader reader = new(filepath);
+            IEnumerable<Wire> wires = await reader.ReadFileAsync();
+            Assert.AreEqual(4, wires.Count());
+        }
+
+        [TestMethod]
+        public async Task ShouldReadFileHeadersAreOutOfOrder()
+        {
+            string directoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resources\";
+            string filepath = directoryPath + "AutoCADFileWithTooManyHeaders.csv";
+            AutoCadFileReader reader = new(filepath);
+            IEnumerable<Wire> wires = await reader.ReadFileAsync();
+            Assert.AreEqual(4, wires.Count());
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfWireNumberIsMissing()
+        {
+            string directoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resources\";
+            string filepath = directoryPath + "AutoCADFileWithMissingWireNumber.csv";
+            AutoCadFileReader reader = new(filepath);
+            Assert.ThrowsExceptionAsync<MissingWireNumberException>(() => reader.ReadFileAsync());
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfAHeaderIsMissing()
+        {
+            string directoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resources\";
+            string filepath = directoryPath + "AutoCADFileWithMissingHeader.csv";
+            AutoCadFileReader reader = new(filepath);
+            Assert.ThrowsExceptionAsync<MissingHeaderException>(() => reader.ReadFileAsync());
+        }
+    }
+}
