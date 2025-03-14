bb45ad879f454baea6e24046b5880e5110ecac4e
Ishmael Moreno <ishmael.a.moreno@gmail.com>
2025-03-14 12:36:57 -0400

    Created test methods for the FileWriters classes.

---
+++ TerminalBlockLabels.Tests/FileWriters/TerminalStripFileWriterWhenCreateTerminalStripFileWriter.cs
@@ -0,0 +1,26 @@
+﻿using CleaverBrooks.Orders;
+using CleaverBrooks.TerminalStripUtility.Library.FileWriters;
+
+namespace CleaverBrooks.TerminalStripUtility.Tests.FileWriters
+{
+    /// <summary>
+    /// Class for testing the constructor of the <see cref="TerminalStripFileWriter"/> class.
+    /// </summary>
+    [TestClass]
+    public class TerminalStripFileWriterWhenCreateTerminalStripFileWriter
+    {
+        [TestMethod]
+        public void ShouldCreate()
+        {
+            List<string> labels = new();
+            SerialNumber serialNumber = new("T1234-1-1");
+            bool isHostPanel = false;
+
+            TerminalStripFileWriter terminalStripFileWriter = new(labels, serialNumber, isHostPanel);
+
+            Assert.AreEqual(labels, terminalStripFileWriter.Labels);
+            Assert.AreEqual(serialNumber, terminalStripFileWriter.SerialNumber);
+            Assert.AreEqual(isHostPanel, terminalStripFileWriter.IsHostPanel);
+        }
+    }
+}
